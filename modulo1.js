aca9a667ba213df822ed199c26f88e77b403bebe
Neto <j.altevirneto@gmail.com>
2024-07-02 09:52:55 -0300

    feat: add funções no modulo

---
+++ modulo1.js
@@ -0,0 +1,15 @@
+function areaCirculo(r){
+    let resultado = 3.14 * (r   ** 2);
+    return resultado;
+}
+const areaTriang = function(b,h){
+    let resultado = (b * h)/2
+    return resultado;
+}
+const areaRetang = (b, h) => b * h;
+
+module.exports = {
+    areaCirculo,
+    areaTriang,
+    areaRetang,
+}
